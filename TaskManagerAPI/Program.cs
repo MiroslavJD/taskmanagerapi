682b0c97259e2c1312f7b66a481d041a9f4cf158
Miroslav <miroslavjd1@gmail.com>
2026-02-21 07:57:11 +0200

    Apply EF Core migrations on startup

--- TaskManagerAPI/Program.cs
+++ TaskManagerAPI/Program.cs
@@ -109,6 +109,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 
 // ======================================================
 // SWAGGER (ALWAYS ENABLED FOR RENDER)
